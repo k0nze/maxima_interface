0cae8ec8a1964c6e294ce333768a3c7f6c8a41a7
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2022-05-26 11:03:35 +0200

    Moved everything into Maxima class and added example.py.

---
+++ example.py
@@ -0,0 +1,8 @@
+from maxima import Maxima
+
+if __name__ == "__main__":
+    m = Maxima(port=65432)
+    
+    print("loop")
+    while True:
+        pass
--- maxima.py
+++ maxima.py
@@ -2,53 +2,45 @@ import socket
 import subprocess
 import threading
 
-def check_if_prompt(data):
-    print(data.decode())
+class Maxima:
+    def __init__(self, port):
+        self.port = port
+        self.start_maxima_server()
+        self.start_maxima()
 
+    def check_if_prompt(self, data):
+        print(data.decode())
 
-def start_maxima_server(port):
-    maxima_server = threading.Thread(target=start_socket_server, args=(port,))
-    maxima_server.deamon = True
-    maxima_server.start()
+    def start_maxima_server(self):
+        maxima_server = threading.Thread(target=self.start_socket_server)
+        maxima_server.deamon = True
+        maxima_server.start()
 
-def start_socket_server(port):
-    HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-    PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+    def start_socket_server(self):
+        HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 
-    print("test")
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, self.port))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    print(f"server: {data}")
+                    #check_if_prompt(data)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, port))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                print(f"server: {data}")
-                #check_if_prompt(data)
+    def start_maxima(self):
+        maxima_thread = threading.Thread(target=self.connect_maxima_to_server)
+        maxima_thread.deamon = True
+        maxima_thread.start()
 
-def start_maxima(port):
-    maxima_thread = threading.Thread(target=connect_maxima_to_server, args=(port,))
-    maxima_thread.deamon = True
-    maxima_thread.start()
+    def connect_maxima_to_server(self):
+        maxima_cmd = f"maxima -s {self.port}"
+        process = subprocess.Popen(maxima_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-def connect_maxima_to_server(port):
-    maxima_cmd = f"maxima -s {port}"
-    process = subprocess.Popen(maxima_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout_iterator = iter(process.stdout.readline, b"")
 
-    stdout_iterator = iter(process.stdout.readline, b"")
-
-    for line in stdout_iterator: 
-        print(f"maxima: {line}")
-
-if __name__ == "__main__":
-    print("Start maxima server")
-    start_maxima_server(65432)
-    print("Start maxima")
-    start_maxima(65432)
-
-    print("loop")
-    while True:
-        pass
+        for line in stdout_iterator: 
+            print(f"maxima: {line}")
