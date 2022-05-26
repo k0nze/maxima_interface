6553b5e9a9ceda490098c866a91fe5656d41489b
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2022-05-26 11:52:21 +0200

    Maxima process is killed now.

--- example.py
+++ example.py
@@ -1,8 +1,12 @@
+import time
+
 from maxima import Maxima
 
 if __name__ == "__main__":
     m = Maxima(port=65432)
-    
-    print("loop")
-    while True:
-        pass
+
+    for i in range(3):
+        print(i)
+        time.sleep(1)
+
+    m.terminate()
--- maxima.py
+++ maxima.py
@@ -1,23 +1,37 @@
 import socket
 import subprocess
 import threading
+import os
+import psutil
+
 
 class Maxima:
     def __init__(self, port):
         self.port = port
+
+        self.maxima_thread = None
+        self.maxima_server_thread = None
+
+        self.maxima_pid = -1
+
         self.start_maxima_server()
         self.start_maxima()
+
+    def terminate(self):
+        # send SIGTERM to maxima sub process
+        self.kill_subprocess(self.maxima_pid)
+        # terminate maxima thread
+        self.maxima_thread.join()
 
     def check_if_prompt(self, data):
         print(data.decode())
 
     def start_maxima_server(self):
-        maxima_server = threading.Thread(target=self.start_socket_server)
-        maxima_server.deamon = True
-        maxima_server.start()
+        self.maxima_server_thread = threading.Thread(target=self.start_socket_server)
+        self.maxima_server_thread.start()
 
     def start_socket_server(self):
-        HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+        HOST = "127.0.0.1"
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((HOST, self.port))
@@ -29,18 +43,31 @@ class Maxima:
                     if not data:
                         break
                     print(f"server: {data}")
-                    #check_if_prompt(data)
+                    # check_if_prompt(data)
 
     def start_maxima(self):
-        maxima_thread = threading.Thread(target=self.connect_maxima_to_server)
-        maxima_thread.deamon = True
-        maxima_thread.start()
+        self.maxima_thread = threading.Thread(target=self.connect_maxima_to_server)
+        self.maxima_thread.start()
 
     def connect_maxima_to_server(self):
         maxima_cmd = f"maxima -s {self.port}"
-        process = subprocess.Popen(maxima_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        process = subprocess.Popen(
+            maxima_cmd,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            preexec_fn=os.setsid,
+        )
+
+        self.maxima_pid = process.pid
 
         stdout_iterator = iter(process.stdout.readline, b"")
 
-        for line in stdout_iterator: 
+        for line in stdout_iterator:
             print(f"maxima: {line}")
+
+    def kill_subprocess(self, pid):
+        process = psutil.Process(pid)
+        for proc in process.children(recursive=True):
+            proc.kill()
+        process.kill()
